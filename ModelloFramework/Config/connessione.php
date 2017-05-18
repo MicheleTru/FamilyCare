265522fbee81820d2dd1c8ec447e39b2508e44be
MicheleTru <michele.truffa@gmail.com>
2017-05-18 20:51:59 +0200

    Update connessione.php

--- ModelloFramework/Config/connessione.php
+++ ModelloFramework/Config/connessione.php
@@ -1,9 +1,9 @@
 <?php
-	$link = mysql_connect('scuola.itisurbino.it:1080', '5a_giommil', 'giommil');
+	$link = mysql_connect('localhost', 'root', 'root');
 	if (!$link){
 		die ('Impossibile connettersi: ' . mysql_error());		
 	}
-	$db_selected = mysql_select_db('5a_giommil', $link);
+	$db_selected = mysql_select_db('root', $link);
 	if (!$db_selected){
 		die ('Impossibile connettersi al database: ' . mysql_error());
 	}
