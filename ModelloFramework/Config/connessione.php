b4fc9a10554969726bdbd16b5c0339c94f396b0e
MicheleTru <michele.truffa@gmail.com>
2017-05-19 12:29:42 +0200

    Update connessione.php

--- ModelloFramework/Config/connessione.php
+++ ModelloFramework/Config/connessione.php
@@ -1,9 +1,9 @@
 <?php
-	$link = mysql_connect('ubuntu@52.42.119.168', 'root', 'root');
+	$link = mysql_connect('localhost', 'phpmyadmin', 'root');
 	if (!$link){
 		die ('Impossibile connettersi: ' . mysql_error());		
 	}
-	$db_selected = mysql_select_db('root', $link);
+	$db_selected = mysql_select_db('phpmyadmin', $link);
 	if (!$db_selected){
 		die ('Impossibile connettersi al database: ' . mysql_error());
 	}
