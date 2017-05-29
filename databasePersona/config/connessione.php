d88123d8e0ffe03f873ee222c40a78cf306d57e6
MicheleTru <michele.truffa@gmail.com>
2017-05-29 11:59:36 +0200

    Update connessione.php

--- databasePersona/config/connessione.php
+++ databasePersona/config/connessione.php
@@ -1,12 +1,12 @@
 <?php
 
-$link = mysql_connect('10.0.0.102', '5a_dilorenzim', 'dilorenzim');
+$link = mysql_connect('localhost', 'root', 'root');
 if (!$link) {
     die('Not connected : ' . mysql_error());
 }
 
 // make foo the current db
-$db_selected = mysql_select_db('5a_dilorenzim', $link);
+$db_selected = mysql_select_db('Progetto', $link);
 if (!$db_selected) {
     die ('Can\'t use foo : ' . mysql_error());
 }
