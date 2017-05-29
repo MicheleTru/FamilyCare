5297a859fed4db56d8bbb15a2dcb87ee61208cc0
MicheleTru <michele.truffa@gmail.com>
2017-05-29 22:24:12 +0200

    Update utentitabella.class.php

--- ProgettoFamilyCare/Model/utentitabella.class.php
+++ ProgettoFamilyCare/Model/utentitabella.class.php
@@ -13,9 +13,9 @@
 								$utente->getUsername(),
 								$utente->getPassword());
 			}
-			mysql_query($query);
+			mysqli_query($query);
 			
-			if(mysql_affected_rows()==1){
+			if(mysqli_affected_rows()==1){
 				return true;
 			}else{
 				return mysql_error();
@@ -50,9 +50,8 @@
 		}
 		public static function getByUsernameAndPassword($user, $pass){
 		$query=sprintf("SELECT * FROM utenti WHERE username='%s' AND password='%s';", $user, sha1($pass));
-		mysql_query($query);
-			print_r(mysql_affected_rows());
-		if(mysql_affected_rows()!=0){
+		mysqli_query($query);
+		if(mysqli_affected_rows()!=0){
 			
 			print('Errore, credenziali errate');
 			return FALSE;
