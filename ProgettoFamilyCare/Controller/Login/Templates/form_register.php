156412d64b472318e6e01432f40c367448e67d10
MicheleTru <michele.truffa@gmail.com>
2017-05-29 14:07:39 +0200

    Update form_register.php

--- ProgettoFamilyCare/Controller/Login/Templates/form_register.php
+++ ProgettoFamilyCare/Controller/Login/Templates/form_register.php
@@ -7,25 +7,34 @@
 	</head>
 	
 	<body>
-		<form action="index.php?controller=login" method="POST">
-			<center>
-			<input type="hidden" name="action" value ="doRegister">
-			<center>
-				Username
-				<br>
-				<input required type="text1" name="username" >
-				<br>
-				<br>
-				Password
-				<br>
-				<input required type="password" name="password" >
-				<br>
-				<br>
-			<input type="submit" class="button" value="Registrati">
-			<br>
-			<br>
-			<p>Una volta registrato, dovrai effettuare il login</p>
-			</center>
-		</form>
+		    <script src="https://use.typekit.net/ayg4pcz.js"></script>
+		    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+		<center>	
+		    <div class="container">
+			
+		    <h1 class="welcome text-center">Welcome to <br> FamilyCare</h1>
+			<div class="card card-container">
+			<h2 class='login_title text-center'>REGISTRATI</h2>
+			<hr>
+		<center>
+			    <form class="form-signin" action="index.php?controller=login" method="POST">
+			        <center>
+					<input type="hidden" name="action" value ="doRegister">
+				<center>
+				<span id="reauth-email" class="reauth-email"></span>
+				<p class="input_title">Utente</p>
+				<input type="text" id="username" class="login_box" required autofocus>
+				<p class="input_title">Password</p>
+				<input type="password" id="password" class="login_box" required>
+				<div id="remember" class="checkbox">
+				    <label>
+
+				    </label>
+				</div>
+				<button class="btn btn-lg btn-primary" type="submit" value="Registrati">Registrati</button>
+				<p>Una volta registrato, dovrai effettuare il login</p>
+			    </form><!-- /form -->
+			</div><!-- /card-container -->
+		    </div><!-- /container -->
 	</body>
 </html>
