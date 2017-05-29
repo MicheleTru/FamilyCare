1e2bfa261a1e405ab89fc2cd914c963932350ee6
MicheleTru <michele.truffa@gmail.com>
2017-05-29 13:53:52 +0200

    Update form.php

--- ProgettoFamilyCare/Controller/Login/Templates/form.php
+++ ProgettoFamilyCare/Controller/Login/Templates/form.php
@@ -2,20 +2,21 @@
 
 <html lang="it">
 	<head>
-		<title>LOGIN</title>
+		<title>Login</title>
 		<meta charset="UTF-8">
 	</head>
 	
 	<body>
 		    <script src="https://use.typekit.net/ayg4pcz.js"></script>
 		    <script>try{Typekit.load({ async: true });}catch(e){}</script>
-
+		<center>	
 		    <div class="container">
+			
 		    <h1 class="welcome text-center">Welcome to <br> FamilyCare</h1>
 			<div class="card card-container">
-			<h2 class='login_title text-center'>Login</h2>
+			<h2 class='login_title text-center'>ACCEDI</h2>
 			<hr>
-
+		</center>
 			    <form class="form-signin" action="index.php?controller=login" method="POST">
 			        <center>
 					<input type="hidden" name="action" value ="doLogin">
