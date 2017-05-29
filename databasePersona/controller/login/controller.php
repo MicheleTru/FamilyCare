c419c4271cbff2ab17a8c11a3289e1901a6b7300
MicheleTru <michele.truffa@gmail.com>
2017-05-29 17:47:03 +0200

    Update controller.php

--- databasePersona/controller/login/controller.php
+++ databasePersona/controller/login/controller.php
@@ -6,14 +6,19 @@
 			$content = get_include_contents("../controller/login/templates/form.php");
 			break;
 		case 'doLogin':
-			$username = UtenteTabella::getByUsernameAndPassword($_POST['username'], $_POST['password']);
-			if ($username != NULL ) {
-				$_SESSION['user_id'] = $username->getId();;
-				header("Location: ?controller=persona&action=list");
-				break;
-			}
-			header("Location: ?controller=login&action=login");
-			break;
+                        $username=NULL;
+
+                        if( UtenteTabella::getByUsernameAndPassword($_POST['username'], sha1 ($_POST['password'])) != NULL){
+                                $username =  UtenteTabella::getByUsernameAndPassword($_POST['username'], sha1 ($_POST['password']));
+
+                                $_SESSION['username'] = $username;
+
+                               echo "Benvenuto";
+                        }else{
+                                $content=get_include_contents('../controller/login/templates/form.php');
+                        }
+                        break;
+
 		case 'doLogout':
 			session_destroy();
 			header("Location: ?controller=login&action=login");
