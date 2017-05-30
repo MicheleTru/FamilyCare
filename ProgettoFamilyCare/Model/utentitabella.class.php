7b29aa1e3d0d76ee28136747b2c233dd66698a9b
MicheleTru <michele.truffa@gmail.com>
2017-05-30 11:37:15 +0200

    Update utentitabella.class.php

--- ProgettoFamilyCare/Model/utentitabella.class.php
+++ ProgettoFamilyCare/Model/utentitabella.class.php
@@ -61,7 +61,7 @@
 		}
 		
 		public static function getByUsername($user){
-			$query = sprintf ("select username from utenti where username=%s", $user);
+			$query = sprintf ("select username from utenti where username=%s;", $user);
 			mysql_query($query);
 			if(mysql_num_rows()>0){
 				return true;
