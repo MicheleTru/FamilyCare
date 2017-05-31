fe9110ccba0ed4efed86842def668840cdbf4ddc
MicheleTru <michele.truffa@gmail.com>
2017-05-31 19:20:08 +0200

    Update controller.php

--- ProgettoFamilyCare/Controller/Login/controller.php
+++ ProgettoFamilyCare/Controller/Login/controller.php
@@ -16,7 +16,7 @@
 			console.log('prova2');
 			if(UtenteTabella::getByUsernameAndPassword($_POST['username'], $_POST['password'])){
 				$_SESSION['username'] = $_POST['username'];
-				$content=get_include_contents('../controller/file.html');
+				echo "Benvenuto " + $_SESSION['username'];
 				break;
 			}			
 			header("Location: ?controller=login&action=login");
